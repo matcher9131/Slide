c9168b47e40b3325d796fdc21f206c1ee8e34842
matcher9131 <matcher9131cd@gmail.com>
2024-01-08 10:11:28 +0900

    Feat: Forbid multiple boot

--- Slide/App.xaml.cs
+++ Slide/App.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Unity;
@@ -18,6 +19,8 @@ namespace Slide
     /// </summary>
     public partial class App : PrismApplication
     {
+        private static Mutex? mutex;
+
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterSingleton<SelectedItemModel>();
@@ -31,12 +34,30 @@ namespace Slide
 
         private void ApplicationStartup(object sender, StartupEventArgs e)
         {
+            mutex = new Mutex(false, "{265A0339-6333-415E-A85E-9B285D2E593E}");
+            if (!mutex.WaitOne(0, false))
+            {
+                // 多重起動
+                MessageBox.Show("既に起動しています。", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
+                mutex.Close();
+                mutex = null;
+                this.Shutdown();
+                return;
+            }
+
             FavoriteLevelDb.Load();
         }
 
         private void ApplicationExit(object sender, ExitEventArgs e)
         {
+            // mutex == nullなら多重起動によるShutdown
+            if (mutex == null) return;
+
             FavoriteLevelDb.Save();
+
+            mutex.ReleaseMutex();
+            mutex.Close();
+            mutex = null;
         }
     }
 }
